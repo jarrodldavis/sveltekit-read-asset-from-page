bfed06e4388c83ae95ae73f96ad0b0fb417debd1
Jarrod Davis <developer@jarrodldavis.com>
2024-02-04 06:22:08 +0000

    add simple text example

---
+++ src/routes/text/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import url from './lorem-ipsum.txt?url';
+
+    console.log('url:', url);
+
+    if (import.meta.env.SSR) {
+        async function read_and_print() {
+            const { read } = await import("$app/server");
+            const response = read(url);
+            const text = await response.text();
+            console.log('text:', text);
+        }
+        read_and_print();
+    }
+</script>
+
+<a download="lorem-ipsum.txt" href={url}>Download Text</a>
